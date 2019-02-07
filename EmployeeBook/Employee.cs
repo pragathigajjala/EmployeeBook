4e6fcfd16b7c91ffc6023cff787cb11080b03022
pragathigajjala <46912681+pragathigajjala@users.noreply.github.com>
2019-02-06 22:12:19 -0800

    Add project files.

---
+++ EmployeeBook/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBook
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public String Title { get; set; }
+
+        public static Employee GetEmployee()
+        {
+            var Employee = new Employee
+            {
+                Name = "John Doe",
+                Title = "CEO"
+            };
+            return Employee;
+        }
+    }
+}
